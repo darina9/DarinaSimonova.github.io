971c864c0f7882ccab14a61f45f08889ac36e669
darina9 <111124490+darina9@users.noreply.github.com>
2024-03-04 17:00:48 +0200

    Update ModalComponent.vue

--- src/components/ModalComponent.vue
+++ src/components/ModalComponent.vue
@@ -129,7 +129,7 @@ export default {
         if (modalForm) {
           modalForm.reset(); // Этот метод обнуляет значения полей формы
         }
-        window.location.href = 'https://gilded-eclair-c21534.netlify.app/';
+        window.location.href = 'https://swisstecnology.netlify.app/';
         // window.location.href = "http://127.0.0.1:5500/";
         return false; // Предотвращаем отправку формы, так как не хотим переходить на сторонний сайт
       }
