87aad8cab6f356ff563efaa121ac05bff774fbe0
darina9 <111124490+darina9@users.noreply.github.com>
2024-03-11 13:13:39 +0200

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -5,29 +5,21 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    currentLanguage: 'ru'
+    currentLanguage: 'ru' // Текущий язык
   },
   mutations: {
+    // Мутация для установки текущего языка
     SET_LANGUAGE(state, language) {
       console.log('Mutation SET_LANGUAGE called with language:', language);
-      state.currentLanguage = language;
+      state.currentLanguage = language;// Устанавливаем текущий язык
     }
   },
   actions: {
+    // Действие для установки языка
     setLanguage({ commit }, language) {
       console.log('Action setLanguage called with language:', language);
-      commit('SET_LANGUAGE', language);
+      commit('SET_LANGUAGE', language);// Вызываем мутацию для установки языка
     }
-  },
- 
+  }
+
 });
-// import Vue from 'vue';
-// import App from '../App.vue';
-
-
-// Vue.config.productionTip = false;
-
-// new Vue({
- 
-//   render: h => h(App),
-// }).$mount('#app');
