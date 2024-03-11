5e58f125f10a09a0acb35dd38c45c0d4f983756f
darina9 <111124490+darina9@users.noreply.github.com>
2024-03-11 13:15:12 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,11 @@
 <template>
   <div id="app">
-    <!-- <MainPage />  -->
     <router-view />
   </div>
 </template>
 
 <script>
-// import MainPage from './view/MainPage.vue'; 
-// import NotFound from './view/NotFound.vue';
-
-
 export default {
-  name: 'App',
-  // components: {
-  //   MainPage, 
-  //   NotFound
-  // },
+  name: "App",
 };
 </script>
-
-<style>
-/* Стили для корневого компонента приложения */
-</style>
